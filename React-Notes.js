5e37c5e580103b23f2a84c864736eafaff1790f5
akhr1997 <ak.hr1997@gmail.com>
2023-08-05 00:18:28 -0400

    Use JSX in functional components

--- React-Notes.js
+++ React-Notes.js
@@ -19,7 +19,7 @@ React.creatElement("div", {id: "first-div"}, "Hello")
  * assiging "root" from DOM as React root
  */
 
-const root = react.creatRoot(document.getElementById("root"))
+const root = React.creatRoot(document.getElementById("root"))
 
 /**
  * render - renders parent inside root
@@ -31,3 +31,34 @@ root.render(parent)
  * start parcel
  */
 // npx parcel index.html
+
+/**
+ * To code in react without doing npm create react app at the begining
+ *      -create normal HTML, CSS, JS files
+ *      -install parcel - npm install -D parcel (-D means it's a devDependencies)
+ *      -install react and reactDOM - npm install react, npm install react-dom
+ *      -run parcel - npx parcel index.html
+ *      -add a "start" scrip in package.json to do "parcel index.html"
+ *      -then do "npm start" and "npm run build"
+ */
+
+/**when we do
+ * const headingUsingJSX = <h1 id="headingUsingJSX">H1 from JSX</h1>;
+ * JavaScript engine doesn't understand this
+ * BABEL transpiles so that react understands it and before it reaches JS Engine which is managed by Parcel or webpack
+ * JSX => BABEL transpiles into React.createElement => ReactElement - JS Object => HTML Element(render)
+ */
+
+/**
+ * JSX is HTML line syntax inside JavaScript
+ * We use className in JSx insatead of HTML class
+ * We can camel casing in JSX instead of hyphen in HTML
+ * wrap in () for multiple lines
+ */
+
+/**
+ * COMPONENTS
+ * Class Based
+ * Functional Based  - Always starts with capital letter
+ * Component Composisition - using one component inside another. Just call the oher component like <.../> inside the other 
+ */
